a8c73aa38bc8c19472061a76b6c94e9651a3e2bd
Victor Camargo <victor.camargo@alliesystems.com>
2024-08-24 10:22:36 -0500

    feature: Settings Files Added
    - Remove LCD dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,17 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
-
+#include "settings.h"
 #include "sensors/temperatureSensor.h"
 #include "controllers/proportional.h"
 
-
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-TemperatureSensor tempSensor(0xAA, A0);
+TemperatureSensor tempSensor(Settings::TEMP_SENSOR_ID, A0);
 ProportionlTemperatureController controller(1);
 
 void setup() {
     Serial.begin(115200);
-    lcd.begin(16, 2);
-    lcd.setCursor(0, 0);
-    lcd.print("Temp Control");
-    delay(2000);
-    lcd.clear();
 }
 
 void loop() {
+    // read_settings 
     tempSensor.takeSample();
     Temperature input = tempSensor.getRead();
     Signal response = controller.control(input, Temperature(25.0, 0x00));
@@ -27,12 +20,6 @@ void loop() {
     Serial.println(input.getValue());
     Serial.print("Output: ");
     Serial.println(response.getValue());
-
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(input.getValue());
-    lcd.setCursor(0, 1);
-    lcd.print(input.getSource());
 
     delay(1000);
 }
