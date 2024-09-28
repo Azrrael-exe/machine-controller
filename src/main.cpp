be67e1d91037057e92149d1c1cc4bd617b2aba77
Victor Camargo <victor.camargo@alliesystems.com>
2024-09-28 10:33:49 -0500

    feat: MQTT producer

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,61 @@
 #include "processJson.h"
 #include "serializers/serializers.h"
 
+#include <WiFiS3.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Azrrael";
+const char* password = "3006750425ABC";
+
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+const char* mqtt_topic_pub = "sda-2024/20240928";
+const char* mqtt_topic_sub = "sda/test-iot/control";
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
 TemperatureSensor tempSensor(Settings::TEMP_SENSOR_ID, A0);
 ProportionlTemperatureController controller(0x00, 0.5);
 
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Intentando conexion MQTT...");
+        String clientId = "ArduinoClient-" + String(random(0xffff), HEX);
+        if (client.connect(clientId.c_str())) {
+            Serial.println("conectado");
+        } else {
+            Serial.print("fallo, rc=");
+            Serial.print(client.state());
+            Serial.println(" intentando de nuevo en 5 segundos");
+            delay(5000);
+        }
+    }
+}
+
+void setup_wifi() {
+    delay(10);
+    Serial.println("Conectando a WiFi...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi conectado");
+    Serial.println("Dirección IP: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup() {
     Serial.begin(115200);
+    setup_wifi();
+    client.setServer(mqtt_server, mqtt_port);
 }
 
 void loop() {
-    
+    if (!client.connected()) {
+        reconnect();
+    }
     tempSensor.takeSample();
     Temperature input = tempSensor.getRead();
     Temperature setPoint = Temperature(25.0, 0xAA);
@@ -26,7 +72,8 @@ void loop() {
     
     String jsonString;
     serializeJson(jsonDoc, jsonString);
-    // Serial.println(jsonString);
+    Serial.println(jsonString);
+    client.publish(mqtt_topic_pub, jsonString.c_str());
 
     BytesSerializer<3> serializer = BytesSerializer<3>(0x7E);
     serializer.addRead(input);
